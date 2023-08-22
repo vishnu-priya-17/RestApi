522c969f6e88cce1107fba5813c1898ab5150dc2
vishnu-priya-17 <108928013+vishnu-priya-17@users.noreply.github.com>
2023-08-22 18:18:08 +0530

    Delete java/com directory

--- java/com/example/day4/repositary/FuelRep.java
+++
@@ -1,9 +0,0 @@
-package com.example.day4.repositary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day4.petrol.PetrolBunk;
-
-public interface FuelRep extends JpaRepository<PetrolBunk,Integer>{
-
-}
--- java/com/example/day5/repositories/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day5.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day5.model.Book;
-
-public interface BookRepo extends JpaRepository<Book,Integer>{
-
-}
